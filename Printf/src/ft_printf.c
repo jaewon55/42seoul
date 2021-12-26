be62e9794f45be6c4b7260b3eb328722f9c5992d
Jaewon Choi <mx757@naver.com>
2021-12-27 03:11:17 +0900

    123

--- Printf/src/ft_printf.c
+++ Printf/src/ft_printf.c
@@ -6,19 +6,42 @@
 /*   By: jaewchoi <jaewchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/19 17:54:16 by jaewchoi          #+#    #+#             */
-/*   Updated: 2021/12/19 17:54:18 by jaewchoi         ###   ########.fr       */
+/*   Updated: 2021/12/27 02:50:55 by jaewchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+static int	get_flag_len(char *start)
+{
+	size_t	i;
+
+	i = 0;
+	while (start[i] && !ft_strchr("cspdiuxX%", start[i]))
+		i++;
+	return (i + 1);
+}
+
 int	ft_printf(const char *format, ...)
 {
-	va_list		ap
+	va_list		ap;
 	size_t		i;
+	int			result;
 
+	result = 0;
+	i = 0;
 	va_start(ap, format);
 	while (format[i])
 	{
 		if (format[i] == '%')
+		{
+			ft_handling_conversion(&format[i], ap, *result);
+			format += get_flag_len(&format[i]);
+			i = 0;
+			continue ;
+		}
+		i++;
 	}
+	va_end(ap);
+	free(temp);
+	return (result);
 }
--- Printf/test.c
+++ Printf/test.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
 #include <limits.h>
 #include <unistd.h>
-int	main()
+#include <stdarg.h>
+// void ft2(va_list ap)
+// {
+// 	printf("%d\n", va_arg(ap, int));
+// }
+
+// void test(int str, ...)
+// {
+// 	va_list ap;
+// 	va_start(ap, str);
+// 	int i = 0;
+// 	while (i < str)
+// 	{
+// 		printf("ap%d : %p\n", i + 1, ap);
+// 		ft2(ap);
+// 		i++;
+// 	}
+// }
+
+int main()
 {
-	write(1, "test\n", 5);
-	return 0;
+	// test(5, 10, 20, 30, 40, 50);
+    printf("%d\n", printf("111%d111%d111%2147483648d\n", 0, 0, 50));
+    return 0;
 }
