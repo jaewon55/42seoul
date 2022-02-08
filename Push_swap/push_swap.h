8a8414db737acdf71ba45ec78eacc202aee7f0f5
Jaewon Choi <mx757@naver.com>
2022-02-08 16:44:39 +0900

    ft_printf

---
+++ Push_swap/push_swap.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaewchoi <jaewchoi@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/08 16:26:08 by jaewchoi          #+#    #+#             */
+/*   Updated: 2022/02/08 16:26:08 by jaewchoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include <unistd.h>
+# include <stdlib.h>
+typedef struct s_stack
+{
+	int *stack_a;
+	int *stack_b;
+}	t_stack;
+
+#endif
