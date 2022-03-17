8bff41c233d9f4a469bb7c64db8743b8f5cde621
jaewon <mx757@naver.com>
2022-03-17 19:13:39 +0900

    wait, waitpid complete

--- 5_Pipex/test.c
+++ 5_Pipex/test.c
@@ -1,16 +1,42 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void sig_handler(int sig)
+{
+	printf("child\tchild caught sig : %d\n", sig);
+}
 
 int main(int ac, char *av[])
 {
-	pid_t pid = fork();
+	int		status;
+	pid_t	pid;
+	pid_t	wait_return;
+
+	pid = fork();
+	if (pid < 0)
+		return (1);
 	if (pid > 0)
 	{
-		printf("child PID in parent : %ld\n", (long)pid);
-		printf("parent PID : %ld\n", (long)getpid());
+		printf("parent\tparent PID : %ld\n", (long)getpid());
+		wait_return = waitpid(pid, &status, WUNTRACED);
+		printf("parent\tstop wait_return is : %ld\n", (long)wait_return);
+		if (WIFSTOPPED(status))
+			printf("parent\tchild stopped, signalnum : %d\n", WSTOPSIG(status));
+		sleep(5);
+		wait_return = waitpid(pid, &status, WCONTINUED | WNOHANG);
+		printf("parent\tcontinue wait_return is : %ld\n", (long)wait_return);
+		if (WIFCONTINUED(status))
+			printf("parent\tchild continued, signalnum : %d\n", WTERMSIG(status));
 	}
 	else if (pid == 0)
-		printf("child PID : %ld\n", (long)getpid());
+	{
+		signal(SIGCONT, sig_handler);
+		printf("child\tchild PID : %ld\n", (long)getpid());
+		while (1);
+	}
 	else
 		perror("fork error : ");
 	return (0);
