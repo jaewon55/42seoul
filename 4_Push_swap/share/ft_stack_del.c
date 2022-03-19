d4b0b577f6d60a065408177c1a14174b3dbab954
jaewon <mx757@naver.com>
2022-03-19 14:33:57 +0900

    push_swap : stack_del Null check

--- 4_Push_swap/share/ft_stack_del.c
+++ 4_Push_swap/share/ft_stack_del.c
@@ -6,7 +6,7 @@
 /*   By: jaewchoi <jaewchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 21:07:21 by jaewchoi          #+#    #+#             */
-/*   Updated: 2022/03/08 18:23:24 by jaewchoi         ###   ########.fr       */
+/*   Updated: 2022/03/19 13:00:00 by jaewchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 #include <stdlib.h>
 void	*ft_stack_del(t_stack *stack)
 {
+	if (!stack)
+		return (NULL);
 	ft_list_del(stack->top_a);
 	ft_list_del(stack->top_b);
 	if (stack->sorted_arr)
