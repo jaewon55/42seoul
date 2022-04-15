67fd889970f2631bfec518a13c3b9f0556a21304
jaewon <mx757@naver.com>
2022-04-16 01:06:45 +0900

    add DataStructure

--- pipex2/share/ft_create_exe_file.c
+++ pipex2/share/ft_create_exe_file.c
@@ -6,7 +6,7 @@
 /*   By: jaewchoi <jaewchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 17:57:27 by jaewchoi          #+#    #+#             */
-/*   Updated: 2022/04/15 03:22:18 by jaewchoi         ###   ########.fr       */
+/*   Updated: 2022/04/15 14:12:04 by jaewchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_create_exe_file(void)
 	char	*hashbang;
 
 	hashbang = "#!/bin/bash\n";
-	fd = open("/tmp/pipe_exe", O_WRONLY | O_CREAT, 0755);
+	fd = open("/tmp/pipe_exe", O_WRONLY | O_CREAT | O_TRUNC, 0755);
 	if (fd < 0)
 		ft_perror();
 	write(fd, hashbang, ft_strlen(hashbang));
