906e4c5e33e079cddb517be23cbe8369066e0c60
jaewchoi <mx757@naver.com>
2022-05-09 15:58:19 +0900

    so_long : img test

--- 6_So_long/test.c
+++ 6_So_long/test.c
@@ -1,16 +1,19 @@
 #include "mlx.h"
+#include <stdio.h>
 
 int main(void)
 {
-	void    *win;
-	void    *img;
-	void    *mlx;
-	int     a;
-	int     b;
+	void	*win;
+	void	*img;
+	void	*mlx;
+	int		a;
+	int		b;
 
 	mlx = mlx_init();
-	win = mlx_new_window(mlx, 1024, 512, "so_long");
-	img = mlx_xpm_to_image(mlx, "./img/mushmom.xpm", &a, &b);
+	win = mlx_new_window(mlx, 1920, 1024, "so_long");
+	img = mlx_xpm_file_to_image(mlx, "./img/tree.xpm", &a, &b);
+	a = 100;
+	b = 100;
 	mlx_put_image_to_window(mlx, win, img, 0, 0);
 	mlx_loop(mlx);
 	return (0);
